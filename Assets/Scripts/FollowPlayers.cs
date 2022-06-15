474ff692c675d57e7d2395ecaf3a611341e02d50
RoberP1 <roberpapeschi@gmail.com>
2022-06-15 16:39:29 -0300

    player dead

--- Assets/Scripts/FollowPlayers.cs
+++ Assets/Scripts/FollowPlayers.cs
@@ -6,16 +6,17 @@ public class FollowPlayers : MonoBehaviour
 {
     public Transform player1;
     public Transform player2;
-    Transform Transform;
-
-    private void Awake()
-    {
-        Transform = GetComponent<Transform>();
-    }
-
 
     private void Update()
     {
         transform.position = ((player1.position - player2.position) / 2 )+ player2.position;
+    }
+    public void Player1Dead()
+    {
+        player1 = player2;
+    }
+    public void Player2Dead()
+    {
+        player2 = player1;
     }
 }
